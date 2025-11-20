e935f6569dec67389f53bb6a1014a51893628147
Frederico de Souza Ramos <fredericosramos2@gmail.com>
2025-11-20 16:55:29 -0300

    Implementa testes para as classes Iphone e Nokia

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone iphone = new Iphone("11988887777", "iPhone 14", "123456789000111", 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine();
+
+Smartphone nokia = new Nokia("11999996666", "Nokia 3310", "987654321000222", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
